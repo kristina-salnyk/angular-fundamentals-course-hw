19a057603f4664ae5ac7e63b3e68eed115d0d758
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-05-23 01:01:10 +0200

    feature(courses): add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { SearchComponent } from './pages/courses/search/search.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    CoursesComponent,
+    SearchComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
