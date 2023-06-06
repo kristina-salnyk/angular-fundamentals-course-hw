7abd5cc0330ecd91cf8efa244994149ca8ae34b5
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-06 13:44:36 +0200

    feature(structure): add changes to project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { SearchComponent } from './pages/courses/search/search.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { LocationComponent } from './location/location.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { InputComponent } from './shared/components/input/input.component';
+import { CourseListComponent } from './pages/courses/course-list/course-list.component';
+import { CourseItemComponent } from './pages/courses/course-list/course-item/course-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BreadcrumbsComponent,
     CoursesComponent,
     SearchComponent,
-    FooterComponent
+    FooterComponent,
+    LogoComponent,
+    LocationComponent,
+    ButtonComponent,
+    InputComponent,
+    CourseListComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule,
