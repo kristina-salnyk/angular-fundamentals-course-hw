847d3c29f7317284c1a2f079e77adf5c8b2f4ab5
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-07 13:15:34 +0200

    bugfix(structure): add modules folders to shared and pages dirs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CoursesComponent } from './pages/courses/courses.component';
-import { SearchComponent } from './pages/courses/search/search.component';
-import { FooterComponent } from './footer/footer.component';
-import { LogoComponent } from './header/logo/logo.component';
-import { LocationComponent } from './location/location.component';
-import { ButtonComponent } from './shared/components/button/button.component';
-import { InputComponent } from './shared/components/input/input.component';
-import { CourseListComponent } from './pages/courses/course-list/course-list.component';
-import { CourseItemComponent } from './pages/courses/course-list/course-item/course-item.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/modules/header/header.component';
+import {CoursesComponent} from './pages/modules/courses/courses.component';
+import {SearchComponent} from './pages/modules/courses/search/search.component';
+import {FooterComponent} from './shared/modules/footer/footer.component';
+import {LogoComponent} from './shared/modules/header/logo/logo.component';
+import {LocationComponent} from './shared/modules/location/location.component';
+import {ButtonComponent} from './shared/components/button/button.component';
+import {InputComponent} from './shared/components/input/input.component';
+import {CourseListComponent} from './pages/modules/courses/course-list/course-list.component';
+import {CourseItemComponent} from './pages/modules/courses/course-list/course-item/course-item.component';
 
 @NgModule({
   declarations: [
@@ -35,4 +35,5 @@ import { CourseItemComponent } from './pages/courses/course-list/course-item/cou
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
