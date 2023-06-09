0589b5c1bf0327e79ecaad202b077d0a3dc3dfda
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-09 23:11:54 +0200

    feature(modules): add shared and pages modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { CoursesComponent } from './pages/courses/courses.component';
-import { SearchComponent } from './pages/courses/search/search.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { LogoComponent } from './shared/header/logo/logo.component';
-import { LocationComponent } from './shared/location/location.component';
-import { ButtonComponent } from './shared/components/button/button.component';
-import { InputComponent } from './shared/components/input/input.component';
-import { CourseListComponent } from './pages/courses/course-list/course-list.component';
-import { CourseItemComponent } from './pages/courses/course-list/course-item/course-item.component';
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    CoursesComponent,
-    SearchComponent,
-    FooterComponent,
-    LogoComponent,
-    LocationComponent,
-    ButtonComponent,
-    InputComponent,
-    CourseListComponent,
-    CourseItemComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
