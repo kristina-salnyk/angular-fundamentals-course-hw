66399a9917e4f4aee8ebe85073dfe814158d1aed
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-12 18:35:03 +0200

    feature(init): add initialization of courses array

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../../core/models/Course.model';
 
@@ -7,25 +7,29 @@ import { Course } from '../../core/models/Course.model';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {
-  courses: Course[] = [
-    {
-      id: '1',
-      title: 'Video Course 1. Name tag',
-      description:
-        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      duration: 88,
-      creationDate: '08/28/2020',
-    },
-    {
-      id: '2',
-      title: 'Video Course 2. Name tag',
-      description:
-        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      duration: 88,
-      creationDate: '08/28/2020',
-    },
-  ];
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit() {
+    this.courses = [
+      {
+        id: '1',
+        title: 'Video Course 1. Name tag',
+        description:
+          "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        duration: 88,
+        creationDate: '08/28/2020',
+      },
+      {
+        id: '2',
+        title: 'Video Course 2. Name tag',
+        description:
+          "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        duration: 88,
+        creationDate: '08/28/2020',
+      },
+    ];
+  }
 
   onLoadMoreClick() {
     console.log('Load more click');
