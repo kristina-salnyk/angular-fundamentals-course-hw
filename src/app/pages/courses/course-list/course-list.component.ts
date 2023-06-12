84add1e2cada463a522b9860300a6461ab0346a6
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-12 15:10:03 +0200

    feature(courses-page): moved courses to courses-page component

--- src/app/pages/courses/course-list/course-list.component.ts
+++ src/app/pages/courses/course-list/course-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Course } from '../../../core/models/Course.model';
 
@@ -8,22 +8,5 @@ import { Course } from '../../../core/models/Course.model';
   styleUrls: ['./course-list.component.scss'],
 })
 export class CourseListComponent {
-  courses: Course[] = [
-    {
-      id: '1',
-      title: 'Video Course 1. Name tag',
-      description:
-        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      duration: 88,
-      creationDate: '08/28/2020',
-    },
-    {
-      id: '2',
-      title: 'Video Course 2. Name tag',
-      description:
-        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      duration: 88,
-      creationDate: '08/28/2020',
-    },
-  ];
+  @Input() courses: Course[] = [];
 }
--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Course } from '../../core/models/Course.model';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  courses: Course[] = [
+    {
+      id: '1',
+      title: 'Video Course 1. Name tag',
+      description:
+        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+      duration: 88,
+      creationDate: '08/28/2020',
+    },
+    {
+      id: '2',
+      title: 'Video Course 2. Name tag',
+      description:
+        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+      duration: 88,
+      creationDate: '08/28/2020',
+    },
+  ];
+}
