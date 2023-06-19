8da71be0e6ae6e9cadbf3ea95ad67f4249361050
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-19 20:26:17 +0200

    feature(tests): add testing of course card in the CourseItemComponent

--- src/app/pages/courses/course-list/course-item/course-item.component.spec.ts
+++ src/app/pages/courses/course-list/course-item/course-item.component.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { CourseItemComponent } from './course-item.component';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { Course } from '../../../../core/models/Course.model';
 
 describe('CourseItemComponent', () => {
   let component: CourseItemComponent;
@@ -20,5 +23,61 @@ describe('CourseItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('course card', () => {
+    let courseDe: DebugElement;
+    let courseEl: HTMLElement;
+    let course: Course;
+    let courseDuration: string;
+    let formatDurationToStringMock: jasmine.SpyObj<CourseItemComponent>;
+
+    beforeEach(() => {
+      courseDe = fixture.debugElement.query(By.css('.course-card'));
+      courseEl = courseDe.nativeElement;
+
+      course = {
+        id: '1',
+        title: 'Video Course 1. Name tag',
+        description:
+          "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        duration: 88,
+        creationDate: '08/28/2020',
+      };
+
+      courseDuration = '1h 28min';
+
+      component.course = course;
+
+      formatDurationToStringMock = jasmine.createSpyObj(
+        'formatDurationToString',
+        ['formatDurationToString']
+      );
+      formatDurationToStringMock.formatDurationToString.and.returnValue(
+        courseDuration
+      );
+
+      spyOn(component, 'formatDurationToString').and.returnValue(
+        formatDurationToStringMock.formatDurationToString(0)
+      );
+
+      fixture.detectChanges();
+    });
+
+    it('should display course title', () => {
+      expect(courseEl.textContent).toContain(course.title);
+    });
+
+    it('should display course description', () => {
+      expect(courseEl.textContent).toContain(course.description);
+    });
+
+    it('should display course duration in the correct format', () => {
+      expect(courseEl.textContent).toContain(courseDuration);
+    });
+
+    it('should display course creation date', () => {
+      expect(courseEl.textContent).toContain(course.creationDate);
+    });
   });
 });
